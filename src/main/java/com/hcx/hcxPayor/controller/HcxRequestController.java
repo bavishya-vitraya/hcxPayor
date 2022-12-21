2788766aa428226a26b773b333212ab971f26513
bavishya-vitraya <bavishya@vitrayatech.com>
2022-12-21 16:26:59 +0530

    route path changes

--- src/main/java/com/hcx/hcxPayor/controller/HcxRequestController.java
+++ src/main/java/com/hcx/hcxPayor/controller/HcxRequestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/hcxPayor/request")
+@RequestMapping("/vitrayamockpayor")
 public class HcxRequestController {
 
     @Autowired
