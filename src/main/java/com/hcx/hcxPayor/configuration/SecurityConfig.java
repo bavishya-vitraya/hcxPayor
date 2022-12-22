a51060cc37cf7fdb4ee9b0c0f314011dbdde99f2
joshima-vitraya <joshima.antonnavis@vitrayatech.com>
2022-12-22 11:53:12 +0530

    changes in context path

--- src/main/java/com/hcx/hcxPayor/configuration/SecurityConfig.java
+++ src/main/java/com/hcx/hcxPayor/configuration/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
                http.csrf().disable().
-               authorizeRequests().antMatchers("/","/payor/user/addUser","/payor/user/login","/vitrayamockpayor/preauth/submit").permitAll()
+               authorizeRequests().antMatchers("/","/payor/user/addUser","/payor/user/login","/preauth/submit").permitAll()
                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
                http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
